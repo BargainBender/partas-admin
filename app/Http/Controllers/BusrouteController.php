0f694abaaf50523705e423269f0c04a6742a5891
Sebastian Martinez <sgmartinezlp@gmail.com>
2023-11-14 23:19:37 +0800

    Added validation for bus routes

--- app/Http/Controllers/BusrouteController.php
+++ app/Http/Controllers/BusrouteController.php
@@ -10,89 +10,105 @@ use Inertia\Inertia;
 
 class BusrouteController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
+  /**
+   * Display a listing of the resource.
+   */
+  public function index()
+  {
 
-        $busroutes = Busroute::all();
-        return Inertia::render('Busroutes/List',[
-            'busroutes' => $busroutes
-        ]);
+    $busroutes = Busroute::all();
+    return Inertia::render('Busroutes/List', [
+      'busroutes' => $busroutes
+    ]);
+  }
+
+  /**
+   * Show the form for creating a new resource.
+   */
+  public function create()
+  {
+    $locations = Location::all();
+    return Inertia::render('Busroutes/Create', [
+      'locations' => $locations,
+      'error' => session('error')
+      ]
+    );
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store()
+  {
+    Request::validate([
+      'origin' => 'required',
+      'destination' => 'required',
+    ]);
+
+    if (Request::get('origin') == Request::get('destination')) {
+      $uniqueIdentifier = time();
+      \error_log('NOT ALLOWED');
+      return to_route('busroutes.create')->with('error', $uniqueIdentifier . ':Origin and destination cannot be the same.');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        $locations=Location::all();
-        return Inertia::render('Busroutes/Create',['locations'=>$locations]);
+    Busroute::create([
+      'origin' => Request::get('origin'),
+      'destination' => Request::get('destination'),
+    ]);
+    return to_route('busroutes')->with('success', 'New Route  created.');
+  }
+
+  /**
+   * Display the specified resource.
+   */
+  public function show()
+  {
+  }
+
+  /**
+   * Show the form for editing the specified resource.
+   */
+  public function edit(Busroute $busroute)
+  {
+    $id = Request::get("id");
+    $busroute = Busroute::find($id);
+    $locations = Location::all();
+    return Inertia::render('Busroutes/Edit', [
+      'busroute' => $busroute,
+      'locations' => $locations,
+      'error' => session('error')
+    ]);
+  }
+
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(Busroute $busroute)
+  {
+    Request::validate([
+      'origin' => 'required',
+      'destination' => 'required',
+    ]);
+
+    if (Request::get('origin') == Request::get('destination')) {
+      $uniqueIdentifier = time();
+      return redirect('busroutes.edit')->with('error', $uniqueIdentifier . ':Origin and destination cannot be the same.');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store()
-    {
-        Request::validate([
-            'origin' => 'required',
-            'destination' => 'required',
-        ]);
+    Busroute::where('id', $busroute->id)
+      ->update([
+        'origin' => Request::get('origin'),
+        'destination' => Request::get('destination')
+      ]);
+    return to_route('busroutes')->with('success', 'Route  updated.');
+  }
 
-
-        Busroute::create([
-            'origin' =>Request::get('origin'),
-            'destination' => Request::get('destination'),
-        ]);
-        return to_route('busroutes')->with('success', 'New Route  created.');
-
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show()
-    {
-
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Busroute $busroute)
-    {
-        $id=Request::get("id");
-        $busroute= Busroute::find($id);
-        $locations= Location::all();
-        return Inertia::render('Busroutes/Edit',['busroute'=>$busroute,'locations'=>$locations]);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Busroute $busroute)
-    {
-        Request::validate([
-            'origin' => 'required',
-            'destination' => 'required',
-        ]);
-
-        Busroute::where('id',$busroute->id)
-        ->update([
-            'origin' =>Request::get('origin'),
-            'destination' => Request::get('destination')
-        ]);
-        return to_route('busroutes')->with('success', 'Route  edited.');
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Busroute $busroute)
-    {
-        Busroute::destroy($busroute->id);
-        to_route('busroutes');
-    }
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(Busroute $busroute)
+  {
+    Busroute::destroy($busroute->id);
+    to_route('busroutes');
+  }
 }
--- resources/js/Pages/Busroutes/Create.vue
+++ resources/js/Pages/Busroutes/Create.vue
@@ -1,63 +1,81 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head,router  } from '@inertiajs/vue3';
-import { defineProps } from 'vue';
+import { Head, router } from '@inertiajs/vue3';
+import { defineProps, watchEffect } from 'vue';
 import TextInput from '@/Components/TextInput.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
-import { reactive,ref } from 'vue'
+import { reactive, ref } from 'vue'
+import { useToast } from 'vue-toast-notification';
+import 'vue-toast-notification/dist/theme-sugar.css';
 
-  const form=reactive({
-      origin: "",
-      destination: "",
-  })
+const $toast = useToast();
 
-  function submit() {
-     
-      router.post(route("busroutes.store"),form);
+const form = reactive({
+  origin: "",
+  destination: "",
+})
+
+function submit() {
+
+  router.post(route("busroutes.store"), form);
+}
+
+const props = defineProps({
+  locations: Object,
+  error: String
+})
+
+watchEffect(() => {
+  if (props.error) {
+    $toast.error(props.error.split(':')[1], {
+      'duration': 5000,
+    });
   }
-  defineProps({
-    locations: Object,
 })
 
 
 </script>
 
 <template>
-    <Head title="Create Bus Route" />
+  <Head title="Create Bus Route" />
 
-    <AuthenticatedLayout>
+  <AuthenticatedLayout>
 
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="overflow-hidden ma-8 w-100  bg-red-200 rounded-lg border shadow-xs">
-                    <div class="overflow-x-auto  m-5">
-                        <form class="w-full max-w-sm"  @submit.prevent="submit">
-                            <div class="mb-4">
-                                <InputLabel for="origin" class="block font-medium text-gray-700">Select Origin:</InputLabel>
-                                <select id="route" v-model="form.origin" class="mt-1 block w-full px-4 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-200 focus:outline-none focus:border-indigo-500">
-                                    <option v-for="location in locations"  :value="location.location">
-                                        {{ location.location }} </option>
-                                 </select>
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="overflow-hidden ma-8 w-100  bg-red-200 rounded-lg border shadow-xs">
+          <div class="overflow-x-auto  m-5">
+            <form class="w-full max-w-sm" @submit.prevent="submit">
+              <div class="mb-4">
+                <InputLabel for="origin" class="block font-medium text-gray-700">Select Origin:</InputLabel>
+                <select id="route" v-model="form.origin"
+                  class="mt-1 block w-full px-4 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-200 focus:outline-none focus:border-indigo-500">
+                  <option v-for="location in locations" :value="location.location">
+                    {{ location.location }} </option>
+                </select>
 
-                                 <InputLabel for="destination" class="block font-medium text-gray-700">Select Destination </InputLabel>
-                                <select id="route" v-model="form.destination" class="mt-1 block w-full px-4 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-200 focus:outline-none focus:border-indigo-500">
-                                    <option v-for="location in locations"  :value="location.location">
-                                        {{ location.location }}</option>
-                                 </select>
-                            </div>
-                                <PrimaryButton type="submit" class="flex items-center bg-blue-500 hover:bg-blue-700 text-white font-semibold px-3 py-2 rounded mx-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-2">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                    </svg>
-                                    Submit
-                                </PrimaryButton>
-                            </form>
-                    </div>
-                </div>
-            </div>
+                <InputLabel for="destination" class="block font-medium text-gray-700">Select Destination </InputLabel>
+                <select id="route" v-model="form.destination"
+                  class="mt-1 block w-full px-4 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-200 focus:outline-none focus:border-indigo-500">
+                  <option v-for="location in locations" :value="location.location">
+                    {{ location.location }}</option>
+                </select>
+              </div>
+              <PrimaryButton type="submit"
+                class="flex items-center bg-blue-500 hover:bg-blue-700 text-white font-semibold px-3 py-2 rounded mx-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                  stroke="currentColor" class="w-6 h-6 mr-2">
+                  <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+                Submit
+              </PrimaryButton>
+            </form>
+          </div>
         </div>
+      </div>
+    </div>
 
-    </AuthenticatedLayout>
-</template>
+</AuthenticatedLayout></template>
